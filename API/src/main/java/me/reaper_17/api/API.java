ac9e2d6a3f290d5c9a4b1198896c549490b529ab
Priyan_17 <101181090+Reaper-1709@users.noreply.github.com>
2023-08-06 15:06:34 +0530

    modularization (fix) (attempt 2)

---
+++ API/src/main/java/me/reaper_17/api/API.java
@@ -0,0 +1,17 @@
+package me.reaper_17.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class API extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
